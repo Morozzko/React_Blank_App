f439d7d100422cbd004d7b6e6bccd424084b5560
Morozzko <58587081+Morozzko@users.noreply.github.com>
2020-06-30 12:11:30 +0300

    Добавлен пункт меню

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+export default function Menu() {
+    return (
+        <nav>
+            <NavLink to="/">Main</NavLink>
+        </nav>
+    )
+
+}
