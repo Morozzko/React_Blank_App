e26a0c347e2a07d14a0d61e083bb37da668d3cd7
Morozzko <58587081+Morozzko@users.noreply.github.com>
2020-09-05 14:38:16 +0300

    Добавлен scss

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./store/store";
+import "./index.css"
 
 ReactDOM.render(
     <BrowserRouter>
