2cf9cd21de87c7799d38d0514fb0e8488f27eb11
Ковалев Константин Александрович (4042876) <k.kovalev@vtb.ru>
2021-04-19 13:36:50 +0300

    useInputHook

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,17 @@
+import {ChangeEvent, useCallback, useState} from 'react';
+
+type UseInputType = {
+    text: string
+    onChangeHandler: (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void
+}
+
+const useInput = (): UseInputType => {
+    const [text, setText] = useState<string>("")
+
+    const onChangeHandler = useCallback((event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setText(event.target.value)
+    },[])
+
+    return {text, onChangeHandler}
+}
+export default useInput
