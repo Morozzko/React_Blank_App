ee2ded6c1b306479171e1f3008c3c81ba1e482ab
Ковалев Константин Александрович (4042876) <k.kovalev@vtb.ru>
2021-04-23 12:55:52 +0300

    HightLightter.tsx

---
+++ src/components/reuseable/HightLighter/HightLightter.tsx
@@ -0,0 +1,60 @@
+import JSUtils from "../../../utils/functions";
+import React from "react";
+
+function getHighlightedText(text: string, highlight: string[] | string,color?:string,backgroundColor?:string) {
+
+
+    function splitByRegex(text: string | string[], reg: string) {
+        if (Array.isArray(text)) {
+            let arr: any[] = []
+            for (let i = 0; i < text.length; i++) {
+                const array = text[i]?.split(new RegExp(`(${reg})`, 'gi'));
+                arr = arr.concat(JSUtils.ArrayDecomposition(array.filter(ele => !!(ele.length))))
+            }
+            return arr
+
+        } else {
+            const array = text?.split(new RegExp(`(${reg})`, 'gi'));
+            return array.filter(ele => !!(ele.length))
+        }
+
+
+    }
+
+
+    if (Array.isArray(highlight)) {
+        let acc = [text]
+
+
+        for (let i = 0; i < highlight.length; i++) {
+            acc =  JSUtils.ArrayDecomposition(splitByRegex(acc, highlight[i]))
+        }
+
+        return (
+            <span>
+                {acc.map((part, i) => {
+                    return (
+                        <span key={i}
+                              style={highlight.find(ele => part.toLowerCase() == ele.toLowerCase()) ? {color: "red"} : {}}>
+                            {part}
+                        </span>
+                    )
+                })}
+            </span>
+        )
+
+    } else {
+        const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+        return (
+            <span>
+                {parts.map((part, i) => {
+                    return (
+                        <span key={i} style={part.toLowerCase() === highlight.toLowerCase() ? {color: "red"} : {}}>
+                            {part}
+                        </span>
+                    )
+                })}
+            </span>
+        )
+    }
+}
