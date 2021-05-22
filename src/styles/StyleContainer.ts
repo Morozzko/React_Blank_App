ae3848f08dddd9a7d20898861c7e1b34614a69d0
Ковалев Константин Александрович (4042876) <k.kovalev@vtb.ru>
2021-05-22 10:20:31 +0300

    StyleWrappers.ts

--- src/styles/StyleContainer.ts
+++ src/styles/StyleContainer.ts
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
 
 interface IStyleContainer {
-  column?: boolean;
-  'font-size'?: string;
-  padding?: string;
-  margin?: string;
-  'default-margin'?: boolean;
-  width?: string;
-  height?: string;
-  'max-height'?: string;
-  'max-width'?: string;
-  'font-weight'?: number;
-  'circle-top'?: boolean;
-  'circle-bottom'?: boolean;
-  'background-color'?: string;
-  pointer?: boolean;
-  'no-padding'?: boolean;
-  middle?: boolean;
-  color?: string;
-  'justify-content'?: 'center' | 'flex-start' | 'stretch' | 'flex-end' | 'space-between';
-  'align-items'?: 'center' | 'flex-start' | 'stretch' | 'flex-end';
+    column?: boolean;
+    'font-size'?: string;
+    padding?: string;
+    margin?: string;
+    'default-margin'?: boolean;
+    width?: string;
+    height?: string;
+    'max-height'?: string;
+    'max-width'?: string;
+    'font-weight'?: number;
+    'circle-top'?: boolean;
+    'circle-bottom'?: boolean;
+    'background-color'?: string;
+    pointer?: boolean;
+    'no-padding'?: boolean;
+    middle?: boolean;
+    color?: string;
+    vw?: number;
+    vh?: number;
+    'justify-content'?: 'center' | 'flex-start' | 'stretch' | 'flex-end' | 'space-between';
+    'align-items'?: 'center' | 'flex-start' | 'stretch' | 'flex-end';
 }
 
 const StyleContainer = styled.div<IStyleContainer>`
@@ -29,8 +31,16 @@ const StyleContainer = styled.div<IStyleContainer>`
   flex-direction: ${props => (props.column ? 'column' : 'row')};
   margin: ${props => (props.margin ? props.margin : props['default-margin'] ? '8px' : '0px')};
   padding: ${props => (props.padding ? props.padding : props['no-padding'] ? '0' : '8px')};
-  width: ${props => props.width && props.width};
-  height: ${props => props.height && props.height};
+  width: ${props => props.width
+    ?
+    props.width
+    :
+    props.vw ? `${props.vw}vw` : ""
+};
+  height: ${props => props.height ?
+    props.height
+    :
+    props.vh ? `${props.vh}vh` : ""};
   max-height: ${props => props['max-height'] && props['max-height']};
   max-width: ${props => props['max-width'] && props['max-width']};
   font-weight: ${props => props['font-weight'] && props['font-weight']};
