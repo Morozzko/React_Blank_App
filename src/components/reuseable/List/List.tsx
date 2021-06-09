d2a8ca82af8e799557560bd683ce3722d07feea0
Ковалев Константин Александрович (4042876) <k.kovalev@vtb.ru>
2021-06-09 16:07:10 +0300

    StyleWrappers.ts

--- src/components/reuseable/List/List.tsx
+++ src/components/reuseable/List/List.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 interface IList<T> {
   items: T[];
   renderItem: (item: T) => React.ReactNode;
-  direction?: 'row' | 'column';
+
 }
 
 export default function List<T>(props: IList<T>) {
   //<RE.List items={items} renderItem={(item)=><itemTemplate key/>}
-  return <div style={{ display: 'flex', flexDirection: props.direction }}>{props.items.map(props.renderItem)}</div>;
+  return <>{props.items.map(props.renderItem)}</>;
 }
