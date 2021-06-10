1641add609defd550f04076c4469107bd63c1d35
Ковалев Константин Александрович (4042876) <k.kovalev@vtb.ru>
2021-06-10 10:45:00 +0300

    error callback

--- src/utils/redux-utils.ts
+++ src/utils/redux-utils.ts
@@ -3,9 +3,9 @@ import { RequestStatusType } from '../feature/Core/App/types';
 import { Condition } from '../feature/Core/Condition';
 
 export const handleSetAppStatus = (status: RequestStatusType, thunkAPI: ThunkAPIType) =>
-  thunkAPI.dispatch(Condition.Actions.setAppStatus({ status }));
+    thunkAPI.dispatch(Condition.Actions.setAppStatus({ status }));
 
-export const ThunkCreator = async (creator: { param?: any; apiMethod: (param: any) => any; status?: number }, thunkAPI: ThunkAPIType) => {
+export const ThunkCreator = async (creator: { param?: any; apiMethod: (param: any) => any; status?: number,errorCallback?:()=>void }, thunkAPI: ThunkAPIType) => {
   creator.status = creator.status ? creator.status : 200;
   handleSetAppStatus('loading', thunkAPI);
   try {
@@ -14,9 +14,12 @@ export const ThunkCreator = async (creator: { param?: any; apiMethod: (param: an
       handleSetAppStatus('succeeded', thunkAPI);
       return res.data;
     } else {
+      creator.errorCallback&&creator.errorCallback()
       return handleAsyncServerAppError(res.data, thunkAPI);
     }
   } catch (error) {
+
+    creator.errorCallback&&creator.errorCallback()
     return handleAsyncServerNetworkError(error, thunkAPI);
   }
 };
