c4ae40399f48831c13531b771a15864251dba886
morozzko <a09.faq@gmail.com>
2021-09-03 13:49:54 +0300

    fix

--- src/feature/Core/App/container/index.tsx
+++ src/feature/Core/App/container/index.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../ui';
 import Providers from '../../Providers';
+import GlobalStyle from '../../../../styles/GlobalStyle';
 
 const Container: FC = props => {
   return (
     <BrowserRouter>
       <Providers>
+        <GlobalStyle/>
         <App {...props} />
       </Providers>
     </BrowserRouter>
