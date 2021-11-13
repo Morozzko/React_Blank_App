0803f07958bd32cdb194a526baea7742cb4b84a6
morozzko <a09.faq@gmail.com>
2021-11-13 09:53:51 +0300

    Prepare for 18 react

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './app/entry'
 
-ReactDOM.render(<App.Container />, document.getElementById('root'))
+const container = document.getElementById('root');
+
+
+ReactDOM.render(<App.Container />, container)
