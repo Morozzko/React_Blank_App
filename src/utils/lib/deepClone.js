2abd39166fb7c2c92b1a8ae430e265b9266262f1
morozzko <a09.faq@gmail.com>
2022-01-06 14:31:44 +0300

    fix

---
+++ src/utils/lib/deepClone.js
@@ -0,0 +1,11 @@
+export function deepClone(obj) {
+  const clObj = {};
+  for(const i in obj) {
+    if (obj[i] instanceof Object) {
+      clObj[i] = deepClone(obj[i]);
+      continue;
+    }
+    clObj[i] = obj[i];
+  }
+  return clObj;
+}
