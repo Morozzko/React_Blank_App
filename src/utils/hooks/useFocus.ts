4a9ce8dc46555d7998db7dbcb2022241f2c42ae9
a09fa <a09.faq@gmail.com>
2022-01-31 18:57:42 +0300

    useFocus.ts

---
+++ src/utils/hooks/useFocus.ts
@@ -0,0 +1,15 @@
+import { MutableRefObject, useRef } from 'react'
+
+type useFocusType = [
+  htmlElRef: MutableRefObject<any>,
+  setFocus: () => void
+]
+
+export const useFocus = (): useFocusType => {
+  const htmlElRef = useRef<any>(null)
+  const setFocus = () => {
+    htmlElRef.current && htmlElRef.current.focus()
+  }
+
+  return [htmlElRef, setFocus]
+}
