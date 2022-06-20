0a6b8725f7d5a3d1384cf3f8a34f48b4d524bc70
Kovalev <VTB4062486@region.vtb.ru>
2022-06-20 09:25:32 +0300

    updates for 18 react

--- src/styles/StyleFabric.ts
+++ src/styles/StyleFabric.ts
@@ -18,14 +18,18 @@ export interface IStyleContainer {
   'z-index'?: number
   absolute?: boolean
   border?: string
+  bottom?: string
   color?: string
   column?: boolean
   height?: string
+  left?: string
   margin?: string
   middle?: boolean
   padding?: string
   pointer?: boolean
   position?: 'fixed' | 'sticky' | 'relative' | 'inherit' | 'static'
+  right?: string
+  top?: string
   vh?: number
   vw?: number
   width?: string
@@ -37,6 +41,7 @@ export const style = (props: IStyleContainer) =>
     background-color: ${props['background-color'] && props['background-color']};
     border-radius: ${props['border-radius'] && props['border-radius']};
     border: ${props.border && props.border};
+    bottom: ${props.bottom && props.bottom};
     box-shadow: ${props['box-shadow'] && props['box-shadow']};
     box-sizing: ${props['box-sizing'] ? props['box-sizing'] : props['no-border-box'] ? '' : 'border-box'};
     color: ${props.color && props.color};
@@ -48,6 +53,7 @@ export const style = (props: IStyleContainer) =>
     font-weight: ${props['font-weight'] && props['font-weight']};
     height: ${props.height ? props.height : props.vh ? `${props.vh}vh` : ''};
     justify-content: ${props['justify-content'] ? props['justify-content'] : props.middle ? 'center' : ''};
+    left: ${props.left && props.left};
     margin: ${props.margin ? props.margin : '0'};
     max-height: ${props['max-height'] && props['max-height']};
     max-width: ${props['max-width'] && props['max-width']};
@@ -55,6 +61,8 @@ export const style = (props: IStyleContainer) =>
     min-width: ${props['min-width'] && props['min-width']};
     padding: ${props.padding ? props.padding : '0'};
     position: ${props.absolute ? 'absolute' : props['position-relative'] ? 'relative' : props.position ? props.position : ''};
+    right: ${props.right && props.right};
+    top: ${props.top && props.top};
     width: ${props.width ? props.width : props.vw ? `${props.vw}vw` : ''};
     z-index: ${props['z-index'] && props['z-index']};
   `
