84f01712f92549f41fa0392abfdf5ab8b4965e2d
Kovalev <VTB4062486@region.vtb.ru>
2022-08-02 14:31:17 +0300

    рефактор

--- src/utils/functions/EventTransfer.ts
+++
@@ -1,8 +0,0 @@
-export const EventTransfer = (payload: { EventName: string; data: object; tag?: string; name: string }) => {
-  const { data, EventName, tag, name } = payload
-  const thisTag = tag ? `${tag}-` : ''
-  const type = `${name}-${thisTag}${EventName}`
-  const Event = new CustomEvent(type, { detail: { data } })
-  dispatchEvent(Event)
-  console.log('Event type:', type, '\nevent.detail.data: ', data)
-}
