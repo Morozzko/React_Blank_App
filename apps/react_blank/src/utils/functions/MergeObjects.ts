d69c32eb0868d03cfa9f8baf95f71e4b3476fe50
Kovalev Konstantin <kkovalev@lanit.ru>
2023-04-01 00:14:48 +0300

    Переход на турборепу

---
+++ apps/react_blank/src/utils/functions/MergeObjects.ts
@@ -0,0 +1,44 @@
+export const MergeObjects = (...objects: object[]) => {
+  let result: Record<string, unknown> = {}
+
+  objects.forEach(obj => {
+    if (typeof obj === 'object' && obj !== null) {
+      Object.keys(obj).forEach((key: string) => {
+        // @ts-ignore
+        const objByPath = obj[key]
+
+        if (typeof objByPath === 'object' && objByPath !== null) {
+          result[key] = MergeObjects(result[key] || {}, objByPath)
+        } else {
+          result[key] = objByPath
+        }
+      })
+    }
+  })
+
+  return result
+}
+
+
+// export function MergeObjects() {
+//   let dst = {},
+//     src,
+//     p,
+//     args = [].splice.call(arguments, 0)
+//   while (args.length > 0) {
+//     src = args.splice(0, 1)[0]
+//     if (toString.call(src) === '[object Object]') {
+//       for (p in src) {
+//         if (src.hasOwnProperty(p)) {
+//           if (toString.call(src[p]) === '[object Object]') {
+//             dst[p] = MergeObjects(dst[p] || {}, src[p])
+//           } else {
+//             dst[p] = src[p]
+//           }
+//         }
+//       }
+//     }
+//   }
+//
+//   return dst
+// }
--- packages/hooks/rollup.config.js
+++ packages/hooks/rollup.config.js
@@ -1,34 +1,33 @@
-const fs = require('fs')
-
-const getFiles = (entry, extensions = [], excludeExtensions = []) => {
-  let fileNames = []
-  const dirs = fs.readdirSync(entry)
-
-  dirs.forEach(dir => {
-    const path = `${entry}/${dir}`
-
-    if (fs.lstatSync(path).isDirectory()) {
-      fileNames = [...fileNames, ...getFiles(path, extensions, excludeExtensions)]
-
-      return
-    }
-
-    if (!excludeExtensions.some(exclude => dir.endsWith(exclude)) && extensions.some(ext => dir.endsWith(ext))) {
-      fileNames.push(path)
-    }
-  })
-
-  return fileNames
-}
-
-import resolve from '@rollup/plugin-node-resolve'
+import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import peerDeps from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
-import { babel } from '@rollup/plugin-babel'
 
-const extensions = ['.js', '.ts', '.jsx', '.tsx']
+// const fs = require('fs')
+//
+// const getFiles = (entry, extensions = [], excludeExtensions = []) => {
+//   let fileNames = []
+//   const dirs = fs.readdirSync(entry)
+//
+//   dirs.forEach(dir => {
+//     const path = `${entry}/${dir}`
+//
+//     if (fs.lstatSync(path).isDirectory()) {
+//       fileNames = [...fileNames, ...getFiles(path, extensions, excludeExtensions)]
+//
+//       return
+//     }
+//
+//     if (!excludeExtensions.some(exclude => dir.endsWith(exclude)) && extensions.some(ext => dir.endsWith(ext))) {
+//       fileNames.push(path)
+//     }
+//   })
+//
+//   return fileNames
+// }
+// const extensions = ['.js', '.ts', '.jsx', '.tsx']
 
 // eslint-disable-next-line import/no-default-export
 export default {
