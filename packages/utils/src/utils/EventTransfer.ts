f107c06a186409cb0ce7143c3260b90698e17040
Kovalev Konstantin <kkovalev@lanit.ru>
2023-04-01 01:00:43 +0300

    Переход на турборепу

---
+++ packages/utils/src/utils/EventTransfer.ts
@@ -0,0 +1,31 @@
+type EventTransferPayloadType = {
+  data: object | string | number | boolean | any[]
+  debug?: boolean
+  name: string | NameType
+}
+
+type NameType = {
+  eventName: string
+  tag?: string
+  mfName: string
+}
+
+export const EventTransfer = ({ data, name, debug }: EventTransferPayloadType) => {
+  let type
+
+  if (typeof name === 'object') {
+    const { tag, mfName, eventName } = name
+    const thisTag = tag ? `${tag}-` : ''
+    type = `${mfName}-${thisTag}${eventName}`
+  } else {
+    type = name
+  }
+
+  const Event = new CustomEvent(type, { detail: { data } })
+
+  dispatchEvent(Event)
+
+  if (debug) {
+    console.log('Event type:', type, '\nevent.detail.data: ', data)
+  }
+}
