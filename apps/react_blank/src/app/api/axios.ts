74b84136752518bc30ec2e41b92d71b182bf7cab
Kovalev Konstantin <kkovalev@lanit.ru>
2023-04-05 22:35:00 +0300

    Добавление мьютекса для 401 ошибок

--- apps/react_blank/src/app/api/axios.ts
+++ apps/react_blank/src/app/api/axios.ts
@@ -1,3 +1,4 @@
+import { Mutex } from 'async-mutex'
 import axios from 'axios'
 
 const baseURL = process.env.REACT_APP_BACKEND_IP
@@ -11,27 +12,49 @@ export const instance = axios.create({
   headers,
 })
 
+// create a new mutex
+const mutex = new Mutex()
+
+// Перехватчики запросов: Начало
 instance.interceptors.request.use(config => {
   config.headers!.Authorization = `Bearer ${localStorage.getItem('token')}`
 
   return config
 })
+// Перехватчики запросов: Конец
 
+// Перехватчики ответов: Начало
 instance.interceptors.response.use(
   config => config,
   async error => {
     const request = error.config
+
+    // Обработка 401 ошибки: Начало
     if (error.response.status === 401 && error.config && error.config.errorsCount !== 5) {
-      request.errorsCount = (request.errorsCount || 1) + 1
-      // await keycloak.updateToken(30).then(() => {
-      //   if (keycloak.idToken) {
-      //     localStorage.setItem('token', keycloak.idToken)
-      //   }
-      // })
+      // Если поток свободен: Начало
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire()
+        try {
+          // here code for refresh token
+        } catch (e) {
+          request.errorsCount = (request.errorsCount || 1) + 1
+        } finally {
+          // release must be called once the mutex should be released again.
+          release()
+        }
+      }
+      // Если поток свободен: Конец
+      else {
+        // wait until the mutex is available without locking it
+        await mutex.waitForUnlock()
+      }
 
       return instance.request(request)
     }
+    // Обработка 401 ошибки: Конец
 
+    // Если ошибка не обработана вернуть ошибку
     throw error
   }
 )
+// Перехватчики ответов: Конец
