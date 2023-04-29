b9ccfaab5f9b0bea5c966a69251a39d9903c6054
Kovalev Konstantin <kkovalev@lanit.ru>
2023-04-29 15:29:53 +0300

    Генерация переводов

--- apps/react_blank/.config.locale.js
+++ apps/react_blank/.config.locale.js
@@ -3,7 +3,7 @@ const path = require('path')
 const localesFolder = './src/features/services/localization/locales'
 
 module.exports = {
-  apiKey: '',
+  apiKey: 'AIzaSyAT2uFOrgZVvZTMmQPk7slY98d80BWcjtk',
   googleSheetID: '19fbHw20NyBTrllrqBJjFD99qGbXvUnurUOU0cNHMYsk',
   sheetName: 'FirstPage',
   fileName: 'common.json',
