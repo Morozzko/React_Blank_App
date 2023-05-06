5f7baded1f39f6a0b8b01fbbec50cf86806807bc
kovalev <kkovalev@lanit.ru>
2023-05-06 17:30:34 +0300

    Ассоциативные структуры данных

--- apps/algorithms/src/structures/4.associative-data-structures/2.set/index.ts
+++ apps/algorithms/src/structures/4.associative-data-structures/2.set/index.ts
@@ -8,6 +8,44 @@ mySet.add(2)
 mySet.add(3)
 mySet.add(1) // Дубликаты не добавляются
 
-console.log(mySet.size) // 3
-console.log(mySet.has(1)) // true
-console.log(mySet.has(4)) // false
+// console.log(mySet.size) // 3
+// console.log(mySet.has(1)) // true
+// console.log(mySet.has(4)) // false
+
+export class MySet<T> {
+	private items: { [key: string]: T } = {}
+
+	private toKey(value: T): string {
+		return JSON.stringify(value)
+	}
+
+	add(value: T): void {
+		const key = this.toKey(value)
+		this.items[key] = value
+	}
+
+	has(value: T): boolean {
+		const key = this.toKey(value)
+
+		return this.items.hasOwnProperty(key)
+	}
+
+	delete(value: T): boolean {
+		const key = this.toKey(value)
+		if (this.items.hasOwnProperty(key)) {
+			delete this.items[key]
+
+			return true
+		}
+
+		return false
+	}
+
+	size(): number {
+		return Object.keys(this.items).length
+	}
+
+	clear(): void {
+		this.items = {}
+	}
+}
--- apps/algorithms/src/structures/4.associative-data-structures/3.map/index.ts
+++ apps/algorithms/src/structures/4.associative-data-structures/3.map/index.ts
@@ -9,6 +9,51 @@ myMap.set('one', 1)
 myMap.set('two', 2)
 myMap.set('three', 3)
 
-console.log(myMap.get('one')) // 1
-console.log(myMap.get('two')) // 2
-console.log(myMap.get('three')) // 3
+// console.log(myMap.get('one')) // 1
+// console.log(myMap.get('two')) // 2
+// console.log(myMap.get('three')) // 3
+
+export class MyMap<K, V> {
+	private items: { [key: string]: [K, V] } = {}
+
+	private toKey(key: K): string {
+		return JSON.stringify(key)
+	}
+
+	set(key: K, value: V): void {
+		const keyStr = this.toKey(key)
+		this.items[keyStr] = [key, value]
+	}
+
+	get(key: K): V | undefined {
+		const keyStr = this.toKey(key)
+		const item = this.items[keyStr]
+
+		return item ? item[1] : undefined
+	}
+
+	has(key: K): boolean {
+		const keyStr = this.toKey(key)
+
+		return this.items.hasOwnProperty(keyStr)
+	}
+
+	delete(key: K): boolean {
+		const keyStr = this.toKey(key)
+		if (this.items.hasOwnProperty(keyStr)) {
+			delete this.items[keyStr]
+
+			return true
+		}
+
+		return false
+	}
+
+	size(): number {
+		return Object.keys(this.items).length
+	}
+
+	clear(): void {
+		this.items = {}
+	}
+}
