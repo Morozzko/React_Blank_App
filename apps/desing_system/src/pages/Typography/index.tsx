3c0f026e734d5525267450be883e300787696984
kovalev <a09.faq@gmail.com>
2023-07-04 00:52:26 +0300

    ds

--- apps/desing_system/src/pages/Typography/index.tsx
+++ apps/desing_system/src/pages/Typography/index.tsx
@@ -4,7 +4,7 @@ type OwnPropertyType = {}
 
 export const TypographyPage: FC<OwnPropertyType> = ({}) => {
   const text =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    'Lorem ipsum dolor sit amet, non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
   return (
     <div className="flex flex-col">
